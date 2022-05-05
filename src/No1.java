120775982187a1e75c430d90398f811b23b2731e
mhpark <happyrichbb@gmail.com>
2022-05-05 23:06:31 +0900

    first commit

---
+++ src/No1.java
@@ -0,0 +1,27 @@
+public class No1 {
+
+        public static int search(int[] nums, int target) {
+             int start = 0;
+             int end = nums.length - 1;
+
+             while(start <= end) {
+                 int mid = (start + end) / 2;
+
+                 if(target == nums[mid]) {
+                     return mid;
+                 }
+                 if(target > nums[mid]) {
+                     start = mid+1;
+                 } else {
+                     end = mid-1;
+                 }
+             }
+             return -1;
+        }
+
+        public static void main(String[] args) {
+
+            System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12},9));
+        }
+
+}
