7054acab08f05b249d8d2b64f9a55ac2b82c0504
mhpark <happyrichbb@gmail.com>
2022-05-12 00:31:59 +0900

    commit

--- src/No278.java
+++ src/No278.java
@@ -2,7 +2,7 @@ public class No278 {
     /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
-    public class Solution extends VersionControl {
+
         public int firstBadVersion(int n) {
             int start = 1;
             int end = n;
@@ -17,5 +17,5 @@ public class No278 {
             }
             return start;
         }
-    }
+
 }
