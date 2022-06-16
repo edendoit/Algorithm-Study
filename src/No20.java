9a7ba220077e8b33cbc04bb5e7d55b0a3185f1eb
mhpark <happyrichbb@gmail.com>
2022-06-16 21:41:58 +0900

    commit

--- src/No20.java
+++ src/No20.java
@@ -4,28 +4,26 @@ import java.util.Stack;
 
 public class No20 {
     public boolean isValid(String s) {
-        char[] sToChar = s.toCharArray();
-        Stack<Character> stack = new Stack<>();
 
-        int count = 0, dCount=0;
-        for(int i=0; i<s.length(); i++) {
-            stack.push(sToChar[i]);
-            if(sToChar[i]=='(' || sToChar[i]=='[' || sToChar[i]=='{') {
-                count++;
-            } else {
-                dCount++;
-            }
-            }
-        if(count != dCount) return false;
-        else {
-            for(int i=0; i<s.length(); i++) {
+            if(s.length() % 2 != 0) return false;
+            Stack<Character> stack = new Stack<>();
 
+            for(char ch : s.toCharArray()) {
+                if(ch == '(' || ch == '{' || ch == '[') {stack.push(ch);}
+                else if(ch == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                }else if(ch == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                    stack.pop();
+                }else if(ch == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                    stack.pop();
+                } else
+                    return false;
             }
+
+            return stack.isEmpty();
         }
 
-return true;
 
     }
 
 
-}
