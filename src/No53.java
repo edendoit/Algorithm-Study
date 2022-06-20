cecc79455bc286a461258061c479433d61107dd4
mhpark <happyrichbb@gmail.com>
2022-06-20 21:43:13 +0900

    commit

---
+++ src/No53.java
@@ -0,0 +1,11 @@
+public class No53 {
+    public int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0], maxEndingHere = nums[0];
+
+        for(int i=1; i<nums.length; i++) {
+            maxEndingHere = Math.max(maxEndingHere+nums[i], nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+}
