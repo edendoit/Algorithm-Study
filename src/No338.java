4974160d30b65e125e50551532d8a656c0a0325f
mhpark <happyrichbb@gmail.com>
2022-06-27 21:26:29 +0900

    commit

--- src/No338.java
+++ src/No338.java
@@ -1,5 +1,5 @@
 public class No338 {
-    public int[] countBits(int num) {
+    public static int[] countBits(int num) {
 
         int[] answer = new int[num+1];
 
@@ -7,12 +7,16 @@ public class No338 {
             answer[0] = 0;
 
         for(int i = 1;i<=num;i++){
-
+            System.out.println("answer[i]="+answer[i]);
             answer[i] = answer[i/2] + i%2;
+            System.out.println("Changed answer[i]="+answer[i]);
 
         }
 
         return answer;
     }
+    public static void main(String[] args) {
+        countBits(4);
+    }
 }
-        }
+
---
+++ src/No543.java
@@ -0,0 +1,19 @@
+public class No543 {
+    int max = 0;
+//    public int diameterOfBinaryTree(TreeNode root) {
+//        maxDiameter(root);
+//        return max;
+//    }
+//
+//    private int maxDiameter(TreeNode root) {
+//        if(root==null) return 0;
+//
+//        int leftCount = maxDiameter(root.left);
+//        int rightCount = maxDiameter(root.right);
+//
+//        max = Math.max(max, leftCount+rightCount);
+//
+//        return Math.max(leftCount, rightCount) +1;
+//
+//    }
+}
