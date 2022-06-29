76799a142f5c18dc7e2924a5612862647a6283fa
mhpark <happyrichbb@gmail.com>
2022-06-29 22:46:40 +0900

    commit

---
+++ src/No160.java
@@ -0,0 +1,12 @@
+public class No160 {
+//    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//        if (headA == null || headB == null) return null;
+//
+//        ListNode pa = headA, pb = headB;
+//        while(pa != pb) {
+//            pa = pa == null ? headB : pa.next;
+//            pb = pb == null ? headA : pb.next;
+//        }
+//        return pa;
+//    }
+}
---
+++ src/No21.java
@@ -0,0 +1,18 @@
+public class No21 {
+//    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//        if(list1 == null) return list2;
+//        if(list2 == null) return list1;
+//
+//
+//        if(list1.val > list2.val) {
+//            list2.next = mergeTwoLists(list2.next, list1);
+//            return list2;
+//        } else {
+//            list1.next = mergeTwoLists(list2, list1.next);
+//            return list1;
+//        }
+//
+//
+//
+//    }
+}
