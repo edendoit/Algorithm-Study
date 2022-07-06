33f22c80b4bdf63bec851b4816da4a9fda179d8a
mhpark <happyrichbb@gmail.com>
2022-07-07 00:00:10 +0900

    commit

---
+++ src/No617.java
@@ -0,0 +1,14 @@
+public class No617 {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 == null) return root2;
+        if(root2 == null) return root1;
+
+        TreeNode root3 = new TreeNode(root1.val + root2.val);
+
+        root3.left = mergeTrees(root1.left, root2.left);
+        root3.right = mergeTrees(root1.right, root2.right);
+
+
+        return root3;
+    }
+}
