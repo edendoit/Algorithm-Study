2e9c3a387792bbf09c9547c5da68cd779bff68ef
mhpark <happyrichbb@gmail.com>
2022-07-13 00:08:20 +0900

    commit

--- src/No695.java
+++ src/No695.java
@@ -4,7 +4,7 @@ public class No695 {
         for(int i = 0; i < grid.length; i++) {
             for(int j=0; j<grid[i].length; j++) {
                 if(grid[i][j] == 1) {
-                    max = Math.max(max, dfs(grid, i, j));
+                    max = dfs(grid, i, j);
                 }
             }
         }
@@ -14,7 +14,7 @@ public class No695 {
 
     public int dfs(int[][] grid, int i, int j) {
         if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j] == 0) {
-            return 0;
+            return -1;
         }
 
         grid[i][j] = 0;
