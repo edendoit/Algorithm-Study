4e855d3a00c46e77d19c964d3b068926dbd54996
mhpark <happyrichbb@gmail.com>
2022-07-14 00:06:53 +0900

    commit

--- src/No994.java
+++ src/No994.java
@@ -1,2 +1,43 @@
 public class No994 {
+    public int orangesRotting(int[][] grid) {
+
+
+        int m = grid.length, n = grid[0].length;
+        int freshOranges = 0;
+        for(int i = 0; i< m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(grid[i][j] == 1) {
+                    ++freshOranges;
+                }
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j] == 2)
+                    queue.add(i*n+j);
+            }
+        }
+
+        int minutes = 0;
+        while(!queue.isEmpty() && freshOranges > 0) {
+            int size = queue.size();
+            for(int i=0; i<size; i++) {
+                int num = queue.poll(), row = num/n, col = num%n;
+                for(int[] direction : directions) {
+                    int x = direction[0] + row, y = direction[1] + col;
+                    if(x>-1 && y>-1 && x<m && y<n && grid[x][y] ==1) {
+                        queue.add(x*n+y);
+                        --freshOranges;
+                        grid[x][y] = 2;
+                    }
+                }
+            }
+
+            ++minutes;
+        }
+        return freshOranges == 0 ? minutes : -1;
 }
+
+// https://www.youtube.com/watch?v=TzoDDOj60zE
