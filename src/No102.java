8ef2ae8c0fd6a94d77410a49268bb24c4349175a
mhpark <happyrichbb@gmail.com>
2022-07-15 01:07:25 +0900

    commit

---
+++ src/No102.java
@@ -0,0 +1,35 @@
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class No102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) { return result;}
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+
+            for(int i=0; i<size; i++) {
+                TreeNode curr = q.poll();
+                level.add(curr.val);
+
+                if(curr.left != null) {
+                    q.add(curr.left);
+                }
+                if(curr.right != null) {
+                    q.add(curr.right);
+                }
+            } result.add(level);
+        }
+        return  result;
+    }
+}
+// https://www.youtube.com/watch?v=sFDNL6r5aDM
+
--- src/No994.java
+++ src/No994.java
@@ -1,34 +1,35 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class No994 {
+    private int[][] directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
     public int orangesRotting(int[][] grid) {
-
-
         int m = grid.length, n = grid[0].length;
         int freshOranges = 0;
-        for(int i = 0; i< m; i++) {
-            for(int j = 0; j<n; j++) {
-                if(grid[i][j] == 1) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
                     ++freshOranges;
                 }
             }
         }
-
         Queue<Integer> queue = new LinkedList<>();
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(grid[i][j] == 2)
-                    queue.add(i*n+j);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2)
+                    queue.add(i * n + j);
             }
         }
-
         int minutes = 0;
-        while(!queue.isEmpty() && freshOranges > 0) {
+        while (!queue.isEmpty() && freshOranges > 0) {
             int size = queue.size();
-            for(int i=0; i<size; i++) {
-                int num = queue.poll(), row = num/n, col = num%n;
-                for(int[] direction : directions) {
+            for (int i = 0; i < size; i++) {
+                int num = queue.poll(), row = num / n, col = num % n;
+                for (int[] direction : directions) {
                     int x = direction[0] + row, y = direction[1] + col;
-                    if(x>-1 && y>-1 && x<m && y<n && grid[x][y] ==1) {
-                        queue.add(x*n+y);
+                    if (x > -1 && y > -1 && x < m && y < n && grid[x][y] == 1) {
+                        queue.add(x * n + y);
                         --freshOranges;
                         grid[x][y] = 2;
                     }
@@ -38,6 +39,7 @@ public class No994 {
             ++minutes;
         }
         return freshOranges == 0 ? minutes : -1;
+    }
 }
 
 // https://www.youtube.com/watch?v=TzoDDOj60zE
