af318392527d05832b9fdbfde18ee4298ceae372
mhpark <happyrichbb@gmail.com>
2022-07-15 22:59:29 +0900

    commit

--- src/No102.java
+++ src/No102.java
@@ -5,31 +5,31 @@ import java.util.List;
 import java.util.Queue;
 
 public class No102 {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(root == null) { return result;}
+   //public List<List<Integer>> levelOrder(TreeNode root) {
+   //    List<List<Integer>> result = new ArrayList<>();
+   //    if(root == null) { return result;}
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
+   //    Queue<TreeNode> q = new LinkedList<>();
+   //    q.offer(root);
 
-        while(!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> level = new ArrayList<>();
+   //    while(!q.isEmpty()) {
+   //        int size = q.size();
+   //        List<Integer> level = new ArrayList<>();
 
-            for(int i=0; i<size; i++) {
-                TreeNode curr = q.poll();
-                level.add(curr.val);
+   //        for(int i=0; i<size; i++) {
+   //            TreeNode curr = q.poll();
+   //            level.add(curr.val);
 
-                if(curr.left != null) {
-                    q.add(curr.left);
-                }
-                if(curr.right != null) {
-                    q.add(curr.right);
-                }
-            } result.add(level);
-        }
-        return  result;
-    }
+   //            if(curr.left != null) {
+   //                q.add(curr.left);
+   //            }
+   //            if(curr.right != null) {
+   //                q.add(curr.right);
+   //            }
+   //        } result.add(level);
+   //    }
+   //    return  result;
+   //}
 }
 // https://www.youtube.com/watch?v=sFDNL6r5aDM
 
