1d306a60f0cc764f4fd440ad7d86496808658473
mhpark <happyrichbb@gmail.com>
2022-08-03 22:39:59 +0900

    commit

---
+++ src/No729.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class No729 {
+    boolean result = true;
+    List<Integer> scale = new ArrayList();
+
+//    public MyCalendar() {
+//        scale.add(new ArrayList<>());
+//    }
+
+    public boolean book(int start, int end) {
+        if (result == true) {
+
+        } else {
+
+        }
+    }
+}
