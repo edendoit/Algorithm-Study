6a23feb14e131c43bfa8c4d9227d167f4361c122
mhpark <happyrichbb@gmail.com>
2022-08-09 00:28:58 +0900

    commit

--- src/No438.java
+++ src/No438.java
@@ -1,17 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class No438 {
-    List<Integer> result = new ArrayList();
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList();
         if (p.length() > s.length()) return result;
-    int[] a = new int[26];
+        int[] a = new int[26];
         for (char ch : p.toCharArray()) {
-        a[ch -'a']++;
-    }
-    int[] b = new int[26];
-        for (int l = 0, r = 0; r < s.length(); r++) {
-        b[s.charAt(r) - 'a']++;
-        if (r - l + 1 == p.length()) {
-            if (Arrays.equals(a, b)) result.add(l);
-            b[s.charAt(l++) -'a']--;
+            a[ch -'a']++;
         }
-    }
+        int[] b = new int[26];
+        for (int l = 0, r = 0; r < s.length(); r++) {
+            b[s.charAt(r) - 'a']++;
+            if (r - l + 1 == p.length()) {
+                if (Arrays.equals(a, b)) result.add(l);
+                b[s.charAt(l++) -'a']--;
+            }
+        }
         return result;
+
+    }
 }
