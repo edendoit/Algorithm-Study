5baaa330de08f853ca7d66a117ec0538eb9bbd2f
mhpark <happyrichbb@gmail.com>
2022-08-09 00:28:04 +0900

    commit

--- src/No438.java
+++ src/No438.java
@@ -1,2 +1,17 @@
 public class No438 {
+    List<Integer> result = new ArrayList();
+        if (p.length() > s.length()) return result;
+    int[] a = new int[26];
+        for (char ch : p.toCharArray()) {
+        a[ch -'a']++;
+    }
+    int[] b = new int[26];
+        for (int l = 0, r = 0; r < s.length(); r++) {
+        b[s.charAt(r) - 'a']++;
+        if (r - l + 1 == p.length()) {
+            if (Arrays.equals(a, b)) result.add(l);
+            b[s.charAt(l++) -'a']--;
+        }
+    }
+        return result;
 }
