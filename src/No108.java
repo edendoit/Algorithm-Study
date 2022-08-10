95c49f7112aca851ba47edf519f33397b9409819
mhpark <happyrichbb@gmail.com>
2022-08-11 02:16:32 +0900

    commit

---
+++ src/No108.java
@@ -0,0 +1,15 @@
+public class No108 {
+    //public TreeNode sortedArrayToBST(int[] nums) {
+    //    return helper_binaryBuilder(nums, 0, nums.length-1);
+    //}
+    //private TreeNode helper_binaryBuilder(int[] nums, int left, int right) {
+    //    if(left > right) return null;
+//
+    //    int m = (right+left) / 2;
+    //    TreeNode root = new TreeNode(nums[m]);
+    //    root.left = helper_binaryBuilder(nums, left, m-1);
+    //    root.right = helper_binaryBuilder(nums, m+1, right);
+    //    return root;
+    //}
+}
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/discuss/2404555/JAVA-oror-Easy-Solution-With-Explanation-oror-100-Faster-Code.
