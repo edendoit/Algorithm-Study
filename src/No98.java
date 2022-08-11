cf0f8a855678dfe946332e58519ce3a04552c40e
mhpark <happyrichbb@gmail.com>
2022-08-11 12:22:34 +0900

    commit

---
+++ src/No98.java
@@ -0,0 +1,15 @@
+public class No98 {
+//    public boolean utilValid(TreeNode root, Integer min, Integer max) {
+//        if(root == null) return true;
+//        if(max != null && root.val >= max || min != null && root.val <= min) return false;
+//
+//        return utilValid(root.left, min, root.val) && utilValid(root.right, root.val, max);
+//
+//    }
+//
+//    public boolean isValidBST(TreeNode root) {
+//        return utilValid(root, null, null);
+//    }
+}
+
+// https://leetcode.com/problems/validate-binary-search-tree/discuss/2409206/**Java%3A-Efficient-Code-TC%3A-O(n)-SC%3A-O(1)%3A%3A%3AEasy-to-undertand%3A**
