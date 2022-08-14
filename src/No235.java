2e239d516b8c9562f036f061613ff8cfa7006802
mhpark <happyrichbb@gmail.com>
2022-08-14 12:55:07 +0900

    commit

---
+++ src/No235.java
@@ -0,0 +1,14 @@
+public class No235 {
+   //public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+   //    if(p.val < root.val && q.val < root.val) {
+   //        return lowestCommonAncestor(root.left, p ,q);
+   //    } else if (p.val > root.val && q.val > root.val) {
+   //        return lowestCommonAncestor(root.right, p, q);
+   //    } else {
+   //        return root;
+   //    }
+   //}
+
+
+}
+ // https://www.youtube.com/watch?v=kulWKd3BUcI
