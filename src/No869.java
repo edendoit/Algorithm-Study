fa18577e8877b16b919d91d77a8595e9623cdfee
mhpark <happyrichbb@gmail.com>
2022-08-30 21:17:42 +0900

    commit

---
+++ src/No869.java
@@ -0,0 +1,15 @@
+public class No869 {
+    public boolean reorderedPowerOf2(int n) {
+        long c = counter(n);
+        for (int i = 0; i < 32; i++)
+            if (counter(1 << i) == c) return true;
+        return false;
+    }
+    public long counter(int N) {
+        long res = 0;
+        for (; N > 0; N /= 10) res += (int)Math.pow(10, N % 10);
+        return res;
+    }
+}
+
+// https://leetcode.com/problems/reordered-power-of-2/discuss/149843/C%2B%2BJavaPython-Straight-Forward
