9bc0a702753af34f4e2202a23bd4d53df267234f
mhpark <happyrichbb@gmail.com>
2022-09-01 23:22:00 +0900

    commit

---
+++ src/No897.java
@@ -0,0 +1,13 @@
+public class No897 {
+//    public TreeNode increasingBST(TreeNode root) {
+//        return increasingBST(root, null);
+//    }
+//    public TreeNode increasingBST(TreeNode root, TreeNode tail) {
+//        if(root == null) return tail;
+//        TreeNode res = increasingBST(root.left, root);
+//        root.left = null;
+//        root.right = increasingBST(root.right, tail);
+//        return res;
+//    }
+}
+// https://leetcode.com/problems/increasing-order-search-tree/discuss/165870/Java-Simple-InOrder-Traversal-with-Explanation
