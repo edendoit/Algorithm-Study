a356d7b9bb033afde416cecab886227dff67e1f7
mhpark <happyrichbb@gmail.com>
2022-10-06 23:18:02 +0900

    commit

---
+++ src/No100.java
@@ -0,0 +1,12 @@
+public class No100 {
+//    public boolean isSameTree(TreeNode p, TreeNode q) {
+//        if(p == null && q == null) return true;
+//        if(p == null || q == null) return false;
+//
+//        if(p.val == q.val) {
+//            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+//        }
+//        return false;
+//    }
+}
+// https://leetcode.com/problems/same-tree/discuss/32687/Five-line-Java-solution-with-recursion
