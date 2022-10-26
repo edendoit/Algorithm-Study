ec23a31940bdade586cdb04d2125f7d4fc75c6e9
mhpark <happyrichbb@gmail.com>
2022-10-26 23:09:32 +0900

    commit

--- src/No438.java
+++ src/No438.java
@@ -21,4 +21,24 @@ public class No438 {
         return result;
 
     }
+
+    public List<Integer> findAnagrams2(String s, String p) {
+        int[] cnt = new int[128];
+        for (char c : p.toCharArray()) cnt[c]++;
+
+        List<Integer> ans = new ArrayList<>();
+        for (int r = 0, l = 0; r < s.length(); ++r) {
+            char c = s.charAt(r);
+            cnt[c]--;
+            while (cnt[c] < 0) { // If number of characters `c` is more than our expectation
+                cnt[s.charAt(l)]++;  // Slide left until cnt[c] == 0
+                l++;
+            }
+
+            if (r - l + 1 == p.length()) { // If we already filled enough `p.length()` chars
+                ans.add(l); // Add left index `l` to our result
+            }
+        }
+        return ans;
+    } // https://leetcode.com/problems/find-all-anagrams-in-a-string/discuss/639309/JavaPython-Sliding-Window-Detail-explanation-Clean-and-Concise
 }
