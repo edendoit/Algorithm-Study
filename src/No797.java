a56279985ac62c19a4d02b57c5f0a0f8a45fe917
mhpark <happyrichbb@gmail.com>
2022-11-01 01:03:38 +0900

    commit

--- src/No797.java
+++ src/No797.java
@@ -26,6 +26,31 @@ public class No797 {
         return result;
 
     }
+// https://www.youtube.com/watch?v=CYnvDzMprdc
+
+    public List<List<Integer>> allPathsSourceTarget2(int[][] graph) {
+        if(graph == null || graph.length == 0) return null;
+        int n = graph.length;
+
+        List<Integer> cur = new ArrayList();
+        List<List<Integer>> answer = new ArrayList();
+        cur.add(0);
+        dfs(graph, 0, n-1, cur, answer);
+
+        return answer;
+    }
+
+    private void dfs(int[][] graph, int source, int target, List<Integer> cur, List<List<Integer>> answer) {
+        if(source == target) {
+            answer.add(new ArrayList<>(cur));
+            return;
+        }
+
+        for(int child : graph[source]) {
+            cur.add(child);
+            dfs(graph, child, target, cur, answer);
+            cur.remove(cur.size()-1);
+        }
+    } // https://leetcode.com/problems/all-paths-from-source-to-target/discuss/297408/Java-DFS-solution-Easy-to-understand-and-Explanation
 }
 
-// https://www.youtube.com/watch?v=CYnvDzMprdc
