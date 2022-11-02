a2160f6c3a356bd66f4c5eec9afed884c1a67ed6
mhpark <happyrichbb@gmail.com>
2022-11-02 23:29:33 +0900

    commit

--- src/No3.java
+++ src/No3.java
@@ -17,5 +17,25 @@ public class No3 {
         return count;
     }
 
+    // Sliding Window
+    public int lengthOfLongestSubstring2(String s) {
+        int n = s.length();
+
+        int res = 0;
+        int[] cnt = new int[128];
+        for(int L=0, R=0; R<n; R++) {
+            cnt[s.charAt(R)]++;
+
+            while(cnt[s.charAt(R)] > 1)
+                cnt[s.charAt(L++)]--;
+
+            res = Math.max(res, R-L+1);
+        }
+
+        return res;
+    }
+    // https://www.youtube.com/watch?v=xzRYqeoPyLk
+
+
 
 }
