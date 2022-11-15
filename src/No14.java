4a26c0908104e170f36a102a795dbfa61a230d3f
mhpark <happyrichbb@gmail.com>
2022-11-15 23:32:10 +0900

    commit

---
+++ src/No14.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class No14 {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        int c = 0;
+        while(c < first.length())
+        {
+            if (first.charAt(c) == last.charAt(c))
+                c++;
+            else
+                break;
+        }
+        return c == 0 ? "" : first.substring(0, c);
+    }
+}
+
+// https://leetcode.com/problems/longest-common-prefix/solutions/721752/java-100-just-compare-two-strings/
