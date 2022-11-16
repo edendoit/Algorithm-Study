81f545a731d2b122e736af3e50e12e671c932643
mhpark <happyrichbb@gmail.com>
2022-11-16 22:55:02 +0900

    commit

--- src/No234.java
+++ src/No234.java
@@ -32,4 +32,36 @@ public class No234 {
 //
 //
 //    }
+
+//    public boolean isPalindrome(ListNode head) {
+//        ListNode fast = head, slow = head;
+//        while(fast != null && fast.next != null) {
+//            fast = fast.next.next;
+//            slow = slow.next;
+//        }
+//        if(fast != null) {
+//            slow = slow.next;
+//        }
+//        slow = reverse(slow);
+//        fast = head;
+//
+//        while(slow != null) {
+//            if(fast.val != slow.val) {
+//                return false;
+//            } fast = fast.next;
+//            slow = slow.next;
+//        }
+//        return true;
+//    }
+//
+//    public ListNode reverse(ListNode head) {
+//        ListNode prev = null;
+//        while(head != null) {
+//            ListNode next = head.next;
+//            head.next = prev;
+//            prev = head;
+//            head = next;
+//        } return prev;
+//    }
+    // https://leetcode.com/problems/palindrome-linked-list/solutions/64501/java-easy-to-understand/?envType=study-plan&id=level-2&orderBy=most_votes
 }
