ea3408049e6876262686b50633d0e0d29b82fe30
mhpark <happyrichbb@gmail.com>
2022-11-29 00:37:07 +0900

    commit

--- src/No19.java
+++ src/No19.java
@@ -1,5 +1,5 @@
 // Leetcode best answer study
-//public class No19 {
+public class No19 {
 //    ListNode start = new ListNode(0);
 //    ListNode slow = start,  fast = start;
 //    slow.next = head;
@@ -15,4 +15,32 @@
 //
 //    slow.next = slow.next.next;
 //    return start.next;
+
+
+
+//public ListNode removeNthFromEnd(ListNode head, int n) {
+//    ListNode result = head;
+//    int i = 0;
+//
+//    while(result != null) {
+//        result= result.next;
+//        i++;
+//    }
+//
+//    if(i==n) {
+//        return head.next;
+//    }
+//
+//    int realN = i - n - 1;
+//    result = head;
+//    for(i=0; i<realN; i++) {
+//        result = result.next;
+//    }
+//    result.next = result.next.next;
+//
+//    return head;
 //}
+    // https://leetcode.com/problems/remove-nth-node-from-end-of-list/solutions/127454/remove-nth-node-from-end-of-list/
+}
+
+
