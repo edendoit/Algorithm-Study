a984e95fe1217e5c82ae974d0a68b6a17547b924
mhpark <happyrichbb@gmail.com>
2022-11-30 00:17:54 +0900

    commit

---
+++ src/No141.java
@@ -0,0 +1,13 @@
+public class No141 {
+//    public boolean hasCycle(ListNode head) {
+//        ListNode fast = head, slow = head;
+//        while(fast != null && fast.next != null) {
+//            slow = slow.next;
+//            fast = fast.next.next;
+//            if(fast == slow) return true;
+//        }
+//        return false;
+//
+//    }
+}
+// https://leetcode.com/problems/linked-list-cycle/solutions/127823/linked-list-cycle/
