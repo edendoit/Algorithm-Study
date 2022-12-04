1c1a1bfdd6479f2051530734bbdd5b837ade9dfb
mhpark <happyrichbb@gmail.com>
2022-12-04 23:11:48 +0900

    commit

---
+++ src/No876.java
@@ -0,0 +1,12 @@
+public class No876 {
+//    public ListNode middleNode(ListNode head) {
+//        ListNode reverse = head, compare = head;
+//
+//        while(reverse != null && reverse.next != null) {
+//            compare = compare.next;
+//            reverse = reverse.next.next;
+//        }
+//        return compare;
+//    }
+}
+// https://leetcode.com/problems/middle-of-the-linked-list/solutions/154715/middle-of-the-linked-list/
