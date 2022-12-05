0a79c1e4dbb4bd07540acfbefdb9d4ee7ed95dce
mhpark <happyrichbb@gmail.com>
2022-12-05 23:14:25 +0900

    commit

---
+++ src/No111.java
@@ -0,0 +1,11 @@
+public class No111 {
+//    public int maxDepth(TreeNode root) {
+//        if(root == null){return 0;}
+//        else {
+//            int left = maxDepth(root.left);
+//            int right = maxDepth(root.right);
+//            return Math.max(left, right)+1;
+//        }
+}
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/solutions/176802/maximum-depth-of-binary-tree/
