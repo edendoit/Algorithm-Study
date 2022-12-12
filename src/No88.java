7cc6c131d4e6b0872831fb81b1dcd1ea606fb6ce
mhpark <happyrichbb@gmail.com>
2022-12-12 23:03:05 +0900

    commit

--- src/No88.java
+++ src/No88.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class No88 {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m-1;
@@ -16,5 +18,12 @@ public class No88 {
 
 
 
+    }
+
+     void merge2(int[] nums1, int m, int[] nums2, int n) {
+        for(int i=0; i<n; i++) {
+            nums1[m+i] = nums2[i];
+        }
+        Arrays.sort(nums1);
     }
 }
