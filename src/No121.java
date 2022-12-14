a09a7eb69169cc873960bf34aa376ae185033b00
mhpark <happyrichbb@gmail.com>
2022-12-14 23:31:53 +0900

    commit

--- src/No121.java
+++ src/No121.java
@@ -13,6 +13,20 @@ public class No121 {
         return maxPrice;
     }
 
+    public int maxProfit2(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0; i<prices.length; i++) {
+            if(prices[i]<minPrice) {
+                minPrice=prices[i];
+            } else if(prices[i]-minPrice > maxProfit) {
+                maxProfit = prices[i]-minPrice;
+            }
+        }
+        return maxProfit;
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{5,4,7,6,1,4};
         maxProfit(nums);
--- src/No53.java
+++ src/No53.java
@@ -8,4 +8,16 @@ public class No53 {
         }
         return maxSoFar;
     }
+
+    public int maxSubArray2(int[] nums) {
+        int cur = nums[0];
+        int maxArr = nums[0];
+
+        for(int i=0; i<nums.length; i++) {
+            int num = nums[i];
+            cur = Math.max(num, num+cur);
+            maxArr = Math.max(maxArr, cur);
+        }
+        return maxArr;
+    }
 }
