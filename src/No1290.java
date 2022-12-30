5886581ea582b9efa39e200027500a8ea9047114
mhpark <happyrichbb@gmail.com>
2022-12-31 04:15:32 +0900

    commit

---
+++ src/No1290.java
@@ -0,0 +1,16 @@
+public class No1290 {
+//    public int getDecimalValue(ListNode head) {
+//        List<Integer> nums = new ArrayList();
+//        while(head != null) {
+//            nums.add(head.val);
+//            head = head.next;
+//        }
+//
+//        int result = 0;
+//        for(int i=0; i<nums.size(); i++){
+//            result += nums.get(i) * (Math.pow(2, nums.size()-i-1));
+//
+//        }
+//        return result;
+//    }
+}
