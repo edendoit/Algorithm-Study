4f5bac5c692dfb0d537f01bd2a8856c5ab504d17
mhpark <happyrichbb@gmail.com>
2023-01-04 00:07:52 +0900

    commit

--- src/No24.java
+++ src/No24.java
@@ -1,14 +1,14 @@
 public class No24 {
-    public ListNode swapPairs(ListNode head) {
-        if(head == null || head.next == null) return head;
-
-        ListNode cur = head;
-        ListNode second = head.next;
-
-        cur.next = swapPairs(second.next);
-        second.next = cur;
-
-        return second;
-
-    }
+//    public ListNode swapPairs(ListNode head) {
+//        if(head == null || head.next == null) return head;
+//
+//        ListNode cur = head;
+//        ListNode second = head.next;
+//
+//        cur.next = swapPairs(second.next);
+//        second.next = cur;
+//
+//        return second;
+//
+//    }
 }
