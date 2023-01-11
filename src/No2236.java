fb4bd3cfee1990dd78d7ccc2fba803f21e6c3fcf
mhpark <happyrichbb@gmail.com>
2023-01-11 23:05:01 +0900

    commit

---
+++ src/No2236.java
@@ -0,0 +1,8 @@
+public class No2236 {
+//    public boolean checkTree(TreeNode root) {
+//        if(root.left.val+root.right.val == root.val) {
+//            return true;
+//        }
+//        return false;
+//    }
+}
