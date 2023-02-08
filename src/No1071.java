69d6224c930bb949439deaf7d8aa2c10dcd00cec
mhpark <happyrichbb@gmail.com>
2023-02-08 23:52:42 +0900

    Greatest Common Divisor of Strings

---
+++ src/No1071.java
@@ -0,0 +1,15 @@
+public class No1071 {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1)) return "";
+
+        int gcdVal = gcd(str1.length(), str2.length());
+
+        return str2.substring(0, gcdVal);
+    }
+
+    public static int gcd(int p, int q) {
+        if(q == 0) return p;
+        else return gcd(q, p%q);
+    }
+}
+// https://leetcode.com/problems/greatest-common-divisor-of-strings/solutions/516621/java-clean-solution-0-ms-100/
