feec4c12f09f19b4a12260a654463f757b97164d
mhpark <happyrichbb@gmail.com>
2023-03-09 23:45:52 +0900

    Permutations

--- src/No46.java
+++ src/No46.java
@@ -3,26 +3,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class No46 {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        permute(result, nums, 0);
-        return result;
-    }
-    private void permute(List<List<Integer>> result, int[] array, int start) {
-        if(start >= array.length) {
-            List<Integer> current = new ArrayList<>();
-            for(int a:array) {
-                current.add(a);
-            }
-            result.add(current);
-        } else {
-            for (int i=start; i<array.length; i++) {
-                swap(array, start, i);
-                permute(result, array, start+1);
-                swap(array, start, i);
-            }
-        }
-    }
+//    public List<List<Integer>> permute(int[] nums) {
+//        List<List<Integer>> result = new ArrayList<>();
+//        permute(result, nums, 0);
+//        return result;
+//    }
+//    private void permute(List<List<Integer>> result, int[] array, int start) {
+//        if(start >= array.length) {
+//            List<Integer> current = new ArrayList<>();
+//            for(int a:array) {
+//                current.add(a);
+//            }
+//            result.add(current);
+//        } else {
+//            for (int i=start; i<array.length; i++) {
+//                swap(array, start, i);
+//                permute(result, array, start+1);
+//                swap(array, start, i);
+//            }
+//        }
+//    }
 
     private void swap(int[] array, int i, int j) {
         int temp = array[i];
