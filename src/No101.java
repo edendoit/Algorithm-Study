93f94508e4eabfc005b2e7f31518e34c229cb28f
mhpark <happyrichbb@gmail.com>
2023-03-10 22:22:32 +0900

    Symmetric Tree(BFS)

--- src/No101.java
+++ src/No101.java
@@ -1,3 +1,6 @@
+import javax.swing.tree.TreeNode;
+import java.util.Queue;
+
 public class No101 {
 //    public boolean isSymmetric(TreeNode root) {
 //        if(root==null) return true;
@@ -9,4 +12,30 @@ public class No101 {
 //        if(p==null || q==null) return false;
 //        return (p.val==q.val) && isMirror(p.left,q.right) && isMirror(p.right,q.left);
 //    }
+
+    // BFS
+//    public boolean isSymmetric2(TreeNode root) {
+//        if (root == null) return true;
+//
+//        Queue<TreeNode> queue = new LinkedList<>();
+//        queue.offer(root.left);
+//        queue.offer(root.right);
+//
+//        while(queue.size() >0) {
+//            TreeNode t1 = queue.poll();
+//            TreeNode t2 = queue.poll();
+//
+//            if(t1 == null && t2 == null) continue;
+//            if(t1 == null || t2 == null) return false;
+//            if(t1.val != t2.val) return false;
+//
+//            queue.offer(t1.left);
+//            queue.offer(t2.right);
+//
+//            queue.offer(t1.right);
+//            queue.offer(t2.left);
+//        }
+//        return true;
+//    }
+    // https://leetcode.com/problems/symmetric-tree/solutions/433170/ismirror-dfs-recursion-one-two-stacks-bfs-queue-solution-in-java/
 }
