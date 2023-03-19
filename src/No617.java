74f245de69537e1122b7b807c2b1acad3f42e436
mhpark <happyrichbb@gmail.com>
2023-03-20 08:20:25 +0900

    Merge Two Binary Trees

--- src/No617.java
+++ src/No617.java
@@ -10,5 +10,16 @@ public class No617 {
 //
 //
 //        return root3;
+
+    // solved myself
+//        if(root1 == null && root2 == null) return null;
+//        if(root1 == null) return root2;
+//        if(root2 == null) return root1;
+//
+//        TreeNode res = new TreeNode(0);
+//        res.val = root1.val+root2.val;
+//        res.left = mergeTrees(root1.left, root2.left);
+//        res.right = mergeTrees(root1.right, root2.right);
+//            return res;
 //    }
 }
