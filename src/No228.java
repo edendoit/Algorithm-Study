f26a66f1ac4da7ae1e89932b7e3f83297a22c1aa
mhpark <happyrichbb@gmail.com>
2023-05-25 01:30:13 +0900

    Summary Ranges

---
+++ src/No228.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class No228 {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+
+        for(int i=0; i<nums.length; i++) {
+            int start = nums[i];
+            while(i+1 < nums.length && nums[i]+1 == nums[i+1]) {
+                i++;
+            }
+            if(start != nums[i]) {
+                res.add(start+"->"+nums[i]);
+            } else {
+                res.add(Integer.toString(start));
+            }
+
+
+        }
+        return res;
+    }
+}
