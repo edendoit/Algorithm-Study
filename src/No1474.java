40370c23b4852124d831e8dfc340ad9556c3a336
mhpark <happyrichbb@gmail.com>
2023-05-30 08:19:43 +0900

    Delete N Nodes After M Nodes of a Linked List

---
+++ src/No1474.java
@@ -0,0 +1,32 @@
+ class ListNode {
+    int val;
+     ListNode next;
+     ListNode() {}
+
+     ListNode(int val) {
+        this.val = val;
+     }
+     ListNode(int val, ListNode next) {
+         this.val = val;
+         this.next = next;
+     }
+}
+public class No1474 {
+    public ListNode deleteNodes(ListNode head, int m, int n) {
+        ListNode cur = head, pre = null;
+        while(cur != null) {
+            int i=m, j=n;
+
+            while(cur != null && i-- >0) {
+                pre = cur;
+                cur = cur.next;
+            }
+
+            while(cur != null && j -- >0) {
+                cur = cur.next;
+            }
+            pre.next = cur;
+        }
+        return head;
+    }
+}
