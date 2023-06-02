1ebfb781fb8a77740e4ba39d1c014df3538dba74
mhpark <happyrichbb@gmail.com>
2023-06-02 10:32:33 +0900

    Straightforward Java solution no fancy data structure

---
+++ src/No604.java
@@ -0,0 +1,31 @@
+public class No604 {
+    class StringIterator {
+        String compressedString;
+        Character cur;
+        int index, cnt;
+        public StringIterator(String compressedString) {
+            this.compressedString = compressedString;
+            this.cur = null;
+            this.index = 0;
+            this.cnt = 0;
+        }
+
+        public char next() {
+            if(cnt == 0) {
+                if(index>=compressedString.length()) return ' ';
+                cur = compressedString.charAt(index++);
+                while(index < compressedString.length() && compressedString.charAt(index) >= '0' && compressedString.charAt(index)<='9') {
+                    cnt = 10 * cnt+(compressedString.charAt(index)-'0');
+                    index++;
+                }
+            }
+            cnt --;
+            return cur;
+        }
+
+        public boolean hasNext() {
+            return index < compressedString.length() || cnt != 0;
+        }
+    }
+}
+// https://leetcode.com/problems/design-compressed-string-iterator/solutions/103872/straightforward-java-solution-no-fancy-data-structure/?envType=study-plan-v2&envId=premium-algo-100
