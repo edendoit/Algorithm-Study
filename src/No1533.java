f3755ad467d4e7158e0491905bf1703ac8b7aa21
mhpark <happyrichbb@gmail.com>
2023-06-06 00:53:43 +0900

    Find the Index of the Large Integer

--- src/No1533.java
+++ src/No1533.java
@@ -3,7 +3,9 @@
              // return 1 if sum(arr[l..r]) > sum(arr[x..y])
              // return 0 if sum(arr[l..r]) == sum(arr[x..y])
              // return -1 if sum(arr[l..r]) < sum(arr[x..y])
-             public default int compareSub(int l, int r, int x, int y) {}
+             public default int compareSub(int l, int r, int x, int y) {
+                 return l;
+             }
 
              // Returns the length of the array
              public default int length() {}
