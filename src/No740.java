03524c3e3af28c60faf6a4dd69e9603592c1517e
mhpark <happyrichbb@gmail.com>
2023-06-10 15:02:46 +0900

    Delete and Earn

---
+++ src/No740.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class No740 {
+    public int deleteAndEarn(int[] nums) {
+        Map<Integer, Integer> points = new HashMap<>();
+        int maxNum = 0;
+
+        // map 에 nums의 숫자 횟수를 key, value로 입력 후 제일 큰 숫자 저장
+        for(int num : nums) {
+            points.put(num, points.getOrDefault(num, 0)+num);
+            maxNum = Math.max(maxNum, num);
+        }
+
+        int[] maxPoints = new int[maxNum+1];
+        maxPoints[1] = points.getOrDefault(1, 0);
+
+        for(int num = 2; num<maxPoints.length; num++) {
+            int gain = points.getOrDefault(num, 0);
+            maxPoints[num] = Math.max(maxPoints[num-1], maxPoints[num-2]+gain);
+        }
+        return maxPoints[maxNum];
+
+    }
+}
